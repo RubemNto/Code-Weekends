9499d7b4c6a5d57b7837964a70b69f625d10ef1d
Rubem Fridolino Christ Neto <56802722+RubemNto@users.noreply.github.com>
2022-01-23 19:39:29 +0000

    HashTables challenge

---
+++ Challenges/HashTables/HashTables.md
@@ -0,0 +1,38 @@
+```
+1. Given a HashTables of user numbers and their grades as an array of 3 numbers, print their average values.
+
+EXAMPLE
+HashTable
+0 -> 1 2 3
+1 -> 1 3 5
+2 -> 2 2 6
+
+GetUserGrades(HashTable)
+return 3, 4.5, 5
+
+2. Given a HasTable of integer pairs, make a function that given a key, change the value the integer paired to the key with another value passed by parameter   
+EXAMPLE
+HashTable
+0 -> 1
+1 -> 2
+2 -> 3
+ChangeItemValue(HashTable, 0, 2)
+return  HashTable
+        0 -> 2
+        1 -> 2
+        2 -> 3
+
+3. Given a Hashtables of string integer pairs, parse the values and return the table
+EXAMPLE
+HashTable
+"0" -> 4
+"1" -> 2
+"2" -> 10
+"3" -> 10
+ParseTable(HashTable)
+return  HashTable
+        "1" -> 2
+        "0" -> 4
+        "2" -> 10
+        "3" -> 10
+```
---
+++ Solutions/HashTable/HashTables.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+template <typename Key, typename Value>
+class HashTable
+{
+    struct node
+    {
+        Key key;
+        Value value;
+        struct node *next;
+    };
+    typedef node *Node;
+
+public:
+    HasTable()
+    {
+        head = NULL;
+    };
+
+    void Add(Key key, Value value)
+    {
+        if (head == NULL)
+        {
+            Push(key, value);
+            return;
+        }
+        Node tempHead = head;
+        Node previous = NULL;
+        while (tempHead != NULL)
+        {
+            previous = tempHead;
+            if (tempHead->key == key)
+            {
+                throw std::invalid_argument("Key already inside HashTable");
+            }
+            tempHead = tempHead->next;
+        }
+        tempHead = previous;
+        Node element = new node;
+        element->value = value;
+        element->key = key;
+        element->next = NULL;
+        tempHead->next = element;
+    };
+
+    void Remove(Key key)
+    {
+        if (head == NULL)
+        {
+            return;
+        }
+        Node tempHead = head;
+        Node previous = NULL;
+        while (tempHead != NULL)
+        {
+            if (tempHead->key == key)
+            {
+                previous->next = tempHead->next;
+                break;
+            }
+            previous = tempHead;
+            tempHead = tempHead->next;
+        }
+    };
+
+    //Challenge 2
+    void ChangeValueOf(Key key, Value value)
+    {
+        if (head == NULL)
+        {
+            return;
+        }
+        Node tempHead = head;
+        while (tempHead != NULL)
+        {
+            if (tempHead->key == key)
+            {
+                tempHead->value = value;
+                break;
+            }
+            tempHead = tempHead->next;
+        }
+    }
+
+    Value GetValue(Key key)
+    {
+        if (head == NULL)
+        {
+            throw std::invalid_argument("non existent table");
+        }
+        Node tempHead = head;
+        while (tempHead != NULL)
+        {
+            if (tempHead->key == key)
+            {
+                return tempHead->value;
+            }
+            tempHead = tempHead->next;
+        }
+        throw std::invalid_argument("Key not found inside HashTable");
+    }
+
+    void PrintTable(){
+        Node tempHead = head;
+        while (tempHead != NULL)
+        {
+            cout<<"Value: "<<tempHead->value<<endl;
+            tempHead = tempHead->next;
+        }        
+    }
+
+    int GetSize(){
+        Node tempHead = head;
+        int counter = 0;
+        while (tempHead != NULL)
+        {
+            counter+=1;
+            tempHead = tempHead->next;
+        }
+        return counter;
+    }
+
+        Node head = NULL;
+
+private:
+    void Push(Key key, Value value)
+    {
+        Node element = new node;
+        element->value = value;
+        element->key = key;
+        element->next = NULL;
+        head = element;
+    }
+};
+
+// Challenge 1 
+void getAverageGrades(HashTable<int, vector<int>> table)
+{
+    while (table.head != NULL)
+    {
+        vector<int> data = table.head->value;
+        float average = (data[0] + data[1] + data[2]) / 3.0f;
+        cout << "Average: " << average << endl;
+        table.head = table.head->next;
+    }
+}
+
+//Challenge 3
+HashTable<string,int> ParseData(HashTable<string,int> table)
+{
+    for (int i = 0; i < table.GetSize(); i++)
+    {
+        for (int b = 0; b < table.GetSize(); b++)
+        {
+            if (i == b)
+                continue;
+            else
+            {
+                if (table.GetValue(to_string(i)) > table.GetValue(to_string(b)))
+                {
+                    int temp = table.GetValue(to_string(i));
+                    table.ChangeValueOf(to_string(i),table.GetValue(to_string(b)));
+                    table.ChangeValueOf(to_string(b),temp);
+                }
+            }
+        }
+    }
+    return table;
+}
+
+int main()
+{
+    //don't do what i did. This is really stupid code and must be avoided at all cost
+    // Please, if needed to use this, be prepared to face the consequences of using a very unprotected HashTable for parsing a string integer pair
+    //Good Luck and keep on coding.
+    HashTable<string, int> tableOfValues;
+    for (int i = 0; i < 11; i++)
+    {
+        tableOfValues.Add(to_string(i),i+i);
+    }
+
+    tableOfValues.PrintTable();    
+    cout<<endl;
+    tableOfValues = ParseData(tableOfValues);
+    tableOfValues.PrintTable();    
+
+    return 0;
+}
